435bb194dc6acfc484bbeeb89e645e55d7e44e0b
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-12-01 23:37:24 +0100

    Remove metrics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,7 @@
 <script lang="ts">
-	import { dev } from '$app/environment'
-
-	// Types
-	import type { PageData } from './$types'
-	import type { Snippet } from 'svelte'
-
-	type Props = {
-		data: PageData
-		children: Snippet
-	}
-
-	let { children }: Props = $props()
+	let { children } = $props()
 </script>
 
-<svelte:head>
-	{#if !dev}
-		<script
-			defer
-			data-domain="llmctx.com"
-			src="/metrics/metrics.js"
-			data-api="/metrics/api/event"
-		></script>
-		<script>
-			window.plausible =
-				window.plausible ||
-				function () {
-					;(window.plausible.q = window.plausible.q || []).push(arguments)
-				}
-		</script>
-	{/if}
-</svelte:head>
-
-<div class="container mx-auto">
+<div>
 	{@render children?.()}
 </div>
